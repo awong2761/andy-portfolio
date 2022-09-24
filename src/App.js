2330831bfcea8a94f74d590abd8d83b294e2fa4a
awong2761 <ndwong36@gmail.com>
2022-09-24 02:10:20 -0700

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import React, { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Andy Wong"
+  }, [])
+
   return (
     <div>
       <NavBar/>
